0c04b3b41e266dc0f05b144892b63a923470652a
Taylor Monacelli <taylormonacelli@gmail.com>
2024-09-08 18:50:16 -0400

    sqlc

---
+++ query.sql
@@ -0,0 +1,21 @@
+-- name: GetLinks :many
+SELECT DISTINCT url,
+                title,
+                MIN(created_at) AS first_seen
+FROM   links
+GROUP  BY url
+ORDER  BY first_seen; 
+
+-- name: CreateLink :one
+INSERT
+or     IGNORE
+into   links
+       (
+              url,
+              title
+       )
+       VALUES
+       (
+              ?,
+              ?
+       )
