55f5585f6192c8d157f5461bea0d11aa5a00d1b8
Taylor Monacelli <taylormonacelli@gmail.com>
2025-06-27 16:33:26 -0400

    fix tests

--- cmd/version.go
+++ cmd/version.go
@@ -1,23 +1,5 @@
 package cmd
 
-import (
-	"fmt"
-
-	"github.com/spf13/cobra"
-
-	"github.com/gkwa/ourport/version"
-)
-
-var versionCmd = &cobra.Command{
-	Use:   "version",
-	Short: "Print the version number of ourport",
-	Long:  `All software has versions. This is ourport's`,
-	Run: func(cmd *cobra.Command, args []string) {
-		buildInfo := version.GetBuildInfo()
-		fmt.Println(buildInfo)
-	},
-}
-
 func init() {
 	rootCmd.AddCommand(versionCmd)
 }
